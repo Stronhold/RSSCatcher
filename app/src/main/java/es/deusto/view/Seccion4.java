d7ead615af9a64ea6a1a468813377bfaf87d9ae5
andresFonquernie <andres.fonquernie@opendeusto.es>
2016-05-18 17:43:34 +0200

    seleccionar imagen de galeria

--- app/src/main/java/es/deusto/view/Seccion4.java
+++ app/src/main/java/es/deusto/view/Seccion4.java
@@ -1,6 +1,9 @@
 package es.deusto.view;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.elpoeta.menulateralslide.R;
 
@@ -16,8 +20,11 @@ import com.elpoeta.menulateralslide.R;
  */
 public class Seccion4 extends Fragment {
 
+    private static final int SELECT_FILE = 0;
+
     EditText eTextName, eTextUrl;
     Button bSave;
+    ImageView img;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,7 +32,22 @@ public class Seccion4 extends Fragment {
 
         View rootView = inflater.inflate(R.layout.seccion4, container, false);
 
+        img = (ImageView) rootView.findViewById(R.id.imagePlaceholder);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
         return rootView;
+
+
+    }
+
+    private void selectImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, SELECT_FILE);
     }
 
 
@@ -36,6 +58,7 @@ public class Seccion4 extends Fragment {
         bSave = (Button) rootView.findViewById(R.id.buttonSave);
         startEvents();
     }
+
     private void startEvents() {
         bSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,5 +66,20 @@ public class Seccion4 extends Fragment {
                 Log.d("asd", "asd");
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.i("ENTRO", "AQUI");
+        if (requestCode == SELECT_FILE) {
+            if (null != data) {
+                Uri selectedImage = data.getData();
+                ImageView imgView = (ImageView) getActivity().findViewById(R.id.imagePlaceholder);
+                imgView.setImageURI(selectedImage);
+                imgView.setTag(selectedImage);
+            }
+        }
     }
 }
